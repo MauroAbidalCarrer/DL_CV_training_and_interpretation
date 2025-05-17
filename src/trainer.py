0c2cf9840ccde2d023ff362087e91721e93daad2
Mauro Abidal Carrer <mauroabidal@yahoo.fr>
2025-05-17 18:16:45 +0000

    Feat: Added an extra call to record_and_display_metrics in training before the loop.

--- src/trainer.py
+++ src/trainer.py
@@ -15,8 +15,9 @@ from torch.utils.data import DataLoader as DL
 class Trainer:
     model:Module
     optimizer:Adam
-    epoch: int = field(default=0, init=False)
+    fig: FigureWidget = field(default=None)
     step: int = field(default=0, init=False)
+    epoch: int = field(default=0, init=False)
     loss:CrossEntropyLoss = field(default_factory=CrossEntropyLoss)
     training_metrics:list[dict] = field(default_factory=list, init=False)
 
@@ -32,15 +33,13 @@ class Trainer:
         return DF.from_records(self.training_metrics)
 
     def _training_loop(self, epochs, train_dl:DL, test_dl:DL, plt_kwargs=None):
-        fig = None
         model_device = next(self.model.parameters()).device
-        # Use epoch instead of epoch.
+        # Use self.epoch instead of for epoch in range(epochs).
         # This avoids resetting new metrics DF lines to the same epoch value in case this method gets recalled.
+        if self.epoch == 0:
+            self.record_and_display_metrics(train_dl, test_dl, plt_kwargs)
         for _ in range(epochs):
-            self.training_metrics.append(self.record_metrics(train_dl, test_dl))
-            if plt_kwargs is not None:
-                fig = self.create_figure_widget(plt_kwargs) if fig is None else fig
-                self.update_figure(fig, plt_kwargs)
+            self.model.train()
             for batch_x, batch_y in train_dl:
                 self.optimizer.zero_grad()
                 batch_x = batch_x.to(model_device)
@@ -51,6 +50,14 @@ class Trainer:
                 self.optimizer.step()
                 self.step += 1
             self.epoch += 1
+            self.record_and_display_metrics(train_dl, test_dl, plt_kwargs)
+
+    def record_and_display_metrics(self, train_dl: DL, test_dl: DL, plt_kwargs: dict):
+        self.training_metrics.append(self.record_metrics(train_dl, test_dl))
+        if plt_kwargs is not None:
+            if self.fig is None:
+                self.create_figure_widget(plt_kwargs)
+            self.update_figure(plt_kwargs)
 
     def record_metrics(self, train_dl: DL, test_dl: DL) -> dict[str, any]:
         with torch.no_grad():
@@ -63,30 +70,31 @@ class Trainer:
                 **self.optimizer.state_dict()["param_groups"][-1],
             }
 
+    @torch.no_grad()
     def metrics_of_dataset(self, data_loader: DL, dl_prefix: str) -> dict:
-        with torch.no_grad():
-            model_device = next(self.model.parameters()).device
-            total_loss = 0
-            total_accuracy = 0
-            for batch_x, batch_y in data_loader:
-                batch_x = batch_x.to(model_device)
-                batch_y = batch_y.to(model_device)
-                batch_y_pred = self.model(batch_x)
-                total_loss += self.loss(batch_y_pred, batch_y).item()
-                total_accuracy += (torch.max(batch_y_pred, 1)[1] == batch_y).sum().item()
-            return {
-                # Divide loss by nb batches
-                dl_prefix + "_loss": total_loss / len(data_loader),
-                # Divide accuracy by nb elements
-                dl_prefix + "_accuracy": total_accuracy / len(data_loader.dataset),
-            }
+        self.model.eval()
+        model_device = next(self.model.parameters()).device
+        total_loss = 0
+        total_accuracy = 0
+        for batch_x, batch_y in data_loader:
+            batch_x = batch_x.to(model_device)
+            batch_y = batch_y.to(model_device)
+            batch_y_pred = self.model(batch_x)
+            total_loss += self.loss(batch_y_pred, batch_y).item()
+            total_accuracy += (torch.max(batch_y_pred, 1)[1] == batch_y).sum().item()
+        return {
+            # Divide loss by nb batches
+            dl_prefix + "_loss": total_loss / len(data_loader),
+            # Divide accuracy by nb elements
+            dl_prefix + "_accuracy": total_accuracy / len(data_loader.dataset),
+        }
 
     def create_figure_widget(self, plt_kwargs: dict) -> FigureWidget:
         df = (
             DF.from_records(self.training_metrics)
             .melt(plt_kwargs["x"], plt_kwargs["y"])
         )
-        fig = (
+        self.fig = (
             line(
                 data_frame=df,
                 y="value",
@@ -97,13 +105,12 @@ class Trainer:
             )
             .update_yaxes(matches=None)
         )
-        fig = FigureWidget(fig)
-        display(fig)
-        return fig
+        self.fig = FigureWidget(self.fig)
+        display(self.fig)
 
-    def update_figure(self, fig: FigureWidget, plt_kwargs: dict):
+    def update_figure(self, plt_kwargs: dict):
         df = DF.from_records(self.training_metrics)
-        with fig.batch_update():
+        with self.fig.batch_update():
             for i, plt_y in enumerate(plt_kwargs["y"]):
-                fig.data[i].x = df[plt_kwargs["x"]]
-                fig.data[i].y = df[plt_y]
+                self.fig.data[i].x = df[plt_kwargs["x"]]
+                self.fig.data[i].y = df[plt_y]
