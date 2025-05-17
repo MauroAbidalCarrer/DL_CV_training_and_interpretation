b23b8db071641c2602a77dc0c5af930eb1e95342
Mauro Abidal Carrer <mauroabidal@yahoo.fr>
2025-05-17 15:54:12 +0000

    Fix: Fixed accuracy estimation in Trainer.metrics_of_dataset.

--- src/trainer.py
+++ src/trainer.py
@@ -53,7 +53,7 @@ class Trainer:
                 self.step += 1
             self.epoch += 1
 
-    def record_metrics(self, train_dl:DL, test_dl:DL) -> dict[str, any]:
+    def record_metrics(self, train_dl: DL, test_dl: DL) -> dict[str, any]:
         with torch.no_grad():
             return {
                 "epoch": self.epoch,
@@ -64,21 +64,24 @@ class Trainer:
                 **self.optimizer.state_dict()["param_groups"][-1],
             }
 
-    def metrics_of_dataset(self, data_loader:DL, dl_prefix:str) -> dict:
+    def metrics_of_dataset(self, data_loader: DL, dl_prefix: str) -> dict:
         model_device = next(self.model.parameters()).device
-        losses = []
-        accuracies = []
+        total_loss = 0
+        total_accuracy = 0
         for batch_x, batch_y in data_loader:
             batch_x = batch_x.to(model_device)
             batch_y = batch_y.to(model_device)
-            losses.append(self.loss(self.model(batch_x), batch_y).item())
-            accuracies.append((torch.max(self.model(batch_x), 1)[1] == batch_y).sum().item())
+            batch_y_pred = self.model(batch_x)
+            total_loss += self.loss(batch_y_pred, batch_y).item()
+            total_accuracy += (torch.max(batch_y_pred, 1)[1] == batch_y).sum().item()
         return {
-            dl_prefix + "_loss": mean(losses),
-            dl_prefix + "_accuracy":mean(accuracies)
+            # Divide loss by nb batches
+            dl_prefix + "_loss": total_loss / len(data_loader),
+            # Divide accuracy by nb elements
+            dl_prefix + "_accuracy": total_accuracy / len(data_loader.dataset),
         }
 
-    def create_figure_widget(self, plt_kwargs:dict) -> FigureWidget:
+    def create_figure_widget(self, plt_kwargs: dict) -> FigureWidget:
         df = (
             DF.from_records(self.training_metrics)
             .melt(plt_kwargs["x"], plt_kwargs["y"])
@@ -98,7 +101,7 @@ class Trainer:
         display(fig)
         return fig
 
-    def update_figure(self, fig:FigureWidget, plt_kwargs:dict):
+    def update_figure(self, fig: FigureWidget, plt_kwargs: dict):
         df = DF.from_records(self.training_metrics)
         with fig.batch_update():
             for i, plt_y in enumerate(plt_kwargs["y"]):
